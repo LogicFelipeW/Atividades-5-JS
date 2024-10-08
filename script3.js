c01e372bba4431b427a89e33a03095323f668a2c
Felipe Wagatsuma <166264810+LogicFelipeW@users.noreply.github.com>
2024-10-08 12:49:16 -0300

    real

--- script3.js
+++ script3.js
@@ -1,13 +1,41 @@
-let carro = []
+let carros = []
 let continuar = true
 
-    while(continuar){
 
-    carro.modelo = prompt("Insira o modelo do carro")
-    carro.marca = prompt("Insira a marca do carro")
-    carro.ano = parseFloat("Insira o ano do carro")
-    carro.valor = parseFloat("Insira o valor do carro")
+while (continuar) {
+    let carro = {
+    modelo: "",
+    marca: "",
+    ano: 0,
+    valor: 0
+  }
 
+  
+  carro.modelo = prompt('Digite o modelo do carro: ')
+  carro.marca = prompt('Digite a marca do carro: ')
+  carro.ano = prompt('Digite o ano do carro: ')
+  carro.valor = parseFloat(prompt('Digite o valor do carro: '))
 
-    prompt("Você gostaria de continuar adicionando dados de carros?")
+  carros.push(carro);
+
+  
+  let resposta = prompt('Deseja adicionar mais um carro? Digite "sim" para continuar ou "não" para parar.')
+
+  if (resposta !== "sim") {
+    continuar = false;
+  }
 }
+
+let carromaior = carros[0]
+
+for (let i = 1; i < carros.length; i++) {
+  if (carros[i].valor > carromaior.valor) {
+    carromaior = carros[i];
+  }
+}
+
+console.log("O carro mais caro é:");
+console.log(`Modelo: ${carromaior.modelo}`);
+console.log(`Marca: ${carromaior.marca}`);
+console.log(`Ano: ${carromaior.ano}`);
+console.log(`Valor: R$ ${carromaior.valor.toFixed(2)}`);
